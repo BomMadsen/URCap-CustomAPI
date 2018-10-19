af7e92680a2ca336c828763458436586a69a0e86
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-10-19 12:12:14 +0200

    Implemented parent node interfaces

--- com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeService.java
+++ com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeService.java
@@ -1,5 +1,45 @@
 package com.jbm.urcap.customAPI.impl;
 
-public class ParentProgramNodeService {
+import java.util.Locale;
+
+import com.ur.urcap.api.contribution.ViewAPIProvider;
+import com.ur.urcap.api.contribution.program.ContributionConfiguration;
+import com.ur.urcap.api.contribution.program.CreationContext;
+import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
+import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeService;
+import com.ur.urcap.api.domain.data.DataModel;
+
+public class ParentProgramNodeService implements SwingProgramNodeService<ParentProgramNodeContribution, ParentProgramNodeView>{
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void configureContribution(ContributionConfiguration configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ParentProgramNodeView createView(ViewAPIProvider apiProvider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ParentProgramNodeContribution createNode(ProgramAPIProvider apiProvider, ParentProgramNodeView view,
+			DataModel model, CreationContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeView.java
+++ com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeView.java
@@ -1,5 +1,16 @@
 package com.jbm.urcap.customAPI.impl;
 
-public class ParentProgramNodeView {
+import javax.swing.JPanel;
+
+import com.ur.urcap.api.contribution.ContributionProvider;
+import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeView;
+
+public class ParentProgramNodeView implements SwingProgramNodeView<ParentProgramNodeContribution>{
+
+	@Override
+	public void buildUI(JPanel panel, ContributionProvider<ParentProgramNodeContribution> provider) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
