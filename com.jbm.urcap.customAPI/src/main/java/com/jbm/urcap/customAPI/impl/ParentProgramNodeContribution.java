1b535ebd10f625e3954f02c7f4eb56f90009f716
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-10-19 13:03:27 +0200

    Added Contribution Constructor

--- com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeContribution.java
+++ com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeContribution.java
@@ -1,10 +1,20 @@
 package com.jbm.urcap.customAPI.impl;
 
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
+import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
+import com.ur.urcap.api.domain.data.DataModel;
 import com.ur.urcap.api.domain.script.ScriptWriter;
 
 public class ParentProgramNodeContribution implements ProgramNodeContribution{
 
+	private final ProgramAPIProvider apiProvider;
+	private final ParentProgramNodeView view;
+	
+	public ParentProgramNodeContribution(ProgramAPIProvider apiProvider, ParentProgramNodeView view) {
+		this.apiProvider = apiProvider;
+		this.view = view;
+	}
+	
 	@Override
 	public void openView() {
 		// TODO Auto-generated method stub
