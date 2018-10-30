3617fbeef8848f19a7a6207a0100e8a77264d716
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-10-30 08:15:42 +0100

    Created builder for Implement button

--- com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeView.java
+++ com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeView.java
@@ -1,6 +1,14 @@
 package com.jbm.urcap.customAPI.impl;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.ur.urcap.api.contribution.ContributionProvider;
@@ -8,10 +16,49 @@ import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeView;
 
 public class ParentProgramNodeView implements SwingProgramNodeView<ParentProgramNodeContribution>{
 
+	private JButton ImplementButton = new JButton();
+	
 	@Override
 	public void buildUI(JPanel panel, ContributionProvider<ParentProgramNodeContribution> provider) {
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
+		panel.add(createBuildChildTreeButtonBox(ImplementButton, provider));
+	}
+	
+	public void setImplementButtonEnabled(boolean enabled) {
+		ImplementButton.setEnabled(enabled);
+		// TODO Also toggle action listener
+		if(enabled) {
+			ImplementButton.setForeground(new Color(0,0,0));
+		} else {
+			ImplementButton.setForeground(new Color(192, 192, 192));
+		}
 	}
 
+	private Box createBuildChildTreeButtonBox(final JButton button, 
+			final ContributionProvider<ParentProgramNodeContribution> provider) {
+		Box box = Box.createHorizontalBox();
+		box.setAlignmentX(Box.CENTER_ALIGNMENT);
+		
+		JLabel label = new JLabel("Push button to insert child node: ");
+		
+		button.setPreferredSize(new Dimension(161, 40));
+		button.setFocusable(false);
+		
+		button.setText("IMPLEMENT");
+		
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				provider.get();	// TODO Call event in Contribution
+			}
+		});
+		
+		box.add(label);
+		box.add(button);
+		
+		return box;
+	}
+	
 }
