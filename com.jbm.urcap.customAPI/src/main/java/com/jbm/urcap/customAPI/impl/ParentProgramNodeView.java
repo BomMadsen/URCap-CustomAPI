604914aec9804d400536e11fa1c207ae76a2ad10
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-10-30 07:53:35 +0100

    Added BoxLayout to parent View

--- com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeView.java
+++ com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeView.java
@@ -1,5 +1,6 @@
 package com.jbm.urcap.customAPI.impl;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 import com.ur.urcap.api.contribution.ContributionProvider;
@@ -9,7 +10,7 @@ public class ParentProgramNodeView implements SwingProgramNodeView<ParentProgram
 
 	@Override
 	public void buildUI(JPanel panel, ContributionProvider<ParentProgramNodeContribution> provider) {
-		// TODO Auto-generated method stub
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
 	}
 
