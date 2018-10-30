5bcc109d6f1051caf998de504650d7e07054967d
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2018-10-30 09:51:00 +0100

    Added methods to add, find and modify child node implementing MyCustomAPI

--- com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeContribution.java
+++ com.jbm.urcap.customAPI/src/main/java/com/jbm/urcap/customAPI/impl/ParentProgramNodeContribution.java
@@ -1,24 +1,114 @@
 package com.jbm.urcap.customAPI.impl;
 
+import java.awt.Color;
+
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
+import com.ur.urcap.api.domain.ProgramAPI;
 import com.ur.urcap.api.domain.data.DataModel;
+import com.ur.urcap.api.domain.program.ProgramModel;
+import com.ur.urcap.api.domain.program.nodes.ProgramNodeFactory;
+import com.ur.urcap.api.domain.program.nodes.builtin.AssignmentNode;
+import com.ur.urcap.api.domain.program.nodes.contributable.URCapProgramNode;
+import com.ur.urcap.api.domain.program.structure.ProgramNodeVisitor;
+import com.ur.urcap.api.domain.program.structure.TreeNode;
+import com.ur.urcap.api.domain.program.structure.TreeStructureException;
 import com.ur.urcap.api.domain.script.ScriptWriter;
+import com.ur.urcap.api.domain.undoredo.UndoRedoManager;
+import com.ur.urcap.api.domain.undoredo.UndoableChanges;
 
 public class ParentProgramNodeContribution implements ProgramNodeContribution{
 
 	private final ProgramAPIProvider apiProvider;
 	private final ParentProgramNodeView view;
+	private final UndoRedoManager undoRedoManager;
 	
 	public ParentProgramNodeContribution(ProgramAPIProvider apiProvider, ParentProgramNodeView view) {
 		this.apiProvider = apiProvider;
 		this.view = view;
+		this.undoRedoManager = this.apiProvider.getProgramAPI().getUndoRedoManager();
+	}
+	
+	private void addChildNode() {
+		ProgramAPI programAPI = apiProvider.getProgramAPI();
+		ProgramModel programModel = programAPI.getProgramModel();
+		final ProgramNodeFactory nf = programModel.getProgramNodeFactory();
+		final TreeNode root = programModel.getRootTreeNode(this);
+		undoRedoManager.recordChanges(new UndoableChanges() {
+			@Override
+			public void executeChanges() {
+				try {
+					root.addChild(nf.createURCapProgramNode(ChildProgramNodeService.class));
+				} catch (TreeStructureException e) {
+					// TODO: handle exception
+				}
+			}
+		});
+	}
+	
+	private boolean childNodeAlreadyExists() {
+		final boolean[] foundChild = new boolean[1];
+		foundChild[0] = false;
+		TreeNode root = apiProvider.getProgramAPI().getProgramModel().getRootTreeNode(this);
+		root.traverse(new ProgramNodeVisitor() {
+			@Override
+			public void visit(URCapProgramNode programNode, int index, int depth) {
+				if(programNode.canGetAs(MyCustomAPI.class)) {
+					foundChild[0] = true;
+				}
+//				super.visit(programNode, index, depth);
+			}
+		});
+		return foundChild[0];
+	}
+	
+	private void setChildNodeColor(final Color color) {
+		// Get the root-node (this program node)
+		TreeNode root = apiProvider.getProgramAPI().getProgramModel().getRootTreeNode(this);
+		// Traverse the program tree, using the ProgramNodeVisitor
+		root.traverse(new ProgramNodeVisitor() {
+			// Override "visit" looking for "URCapProgramNode" nodes
+			// Visit will be called for every URCapProgramNode in sub-tree
+			@Override
+			public void visit(URCapProgramNode programNode, int index, int depth) {
+				// Check if the found program node implements the "MyCustomAPI" interface
+				if(programNode.canGetAs(MyCustomAPI.class)) {
+					// If it did, we found the right node
+					// Now we can use "getAs" since we already tested with "canGetAs"
+					// After "getAs" we can now call the interface methods 
+					programNode.getAs(MyCustomAPI.class).setColor(color);
+				}
+//				super.visit(programNode, index, depth);
+			}
+		});
+	}
+	
+	private Color getChildNodeColor() {
+		final Color[] color = new Color[1];
+		// Get the root-node (this program node)
+		TreeNode root = apiProvider.getProgramAPI().getProgramModel().getRootTreeNode(this);
+		// Traverse the program tree, using the ProgramNodeVisitor
+		root.traverse(new ProgramNodeVisitor() {
+			// Override "visit" looking for "URCapProgramNode" nodes
+			// Visit will be called for every URCapProgramNode in sub-tree
+			@Override
+			public void visit(URCapProgramNode programNode, int index, int depth) {
+				// Check if the found program node implements the "MyCustomAPI" interface
+				if(programNode.canGetAs(MyCustomAPI.class)) {
+					// If it did, we found the right node
+					// Now we can use "getAs" since we already tested with "canGetAs"
+					// After "getAs" we can now call the interface methods 
+					color[0] = programNode.getAs(MyCustomAPI.class).getColor();
+				}
+//				super.visit(programNode, index, depth);
+			}
+		});
+		return color[0];
 	}
 	
 	@Override
 	public void openView() {
-		// TODO Auto-generated method stub
-		
+		view.setImplementButtonEnabled(!childNodeAlreadyExists());
 	}
 
 	@Override
@@ -41,8 +131,7 @@ public class ParentProgramNodeContribution implements ProgramNodeContribution{
 
 	@Override
 	public void generateScript(ScriptWriter writer) {
-		// TODO Auto-generated method stub
-		
+		writer.writeChildren();
 	}
 
 }
